0fb87ab8973f1781f402f73fa1414fb5f7d06bce
emaenriquez <enriquezemanuel51@gmail.com>
2022-12-02 16:08:23 -0300

    nuevos cambios y archivos incluidos

---
+++ archivo.sql
@@ -0,0 +1,1 @@
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';FLUSH PRIVILEGES;
--- mis ejercicios/practicas.sql
+++ mis ejercicios/practicas.sql
@@ -35,3 +35,68 @@ select * from alumno
 	left join localidad on
 	alumno.IDLOCALIDAD = localidad.IDLOCALIDAD;
 
+-- sentencia if    
+select alu.NOMBREYAPELLIDO ,
+	if(alu.IDLOCALIDAD is null,'sin asignar',loc.localidad) 
+    localidad from alumno alu left join localidad loc on alu.IDLOCALIDAD = loc.IDLOCALIDAD
+    order by alu.NOMBREYAPELLIDO;
+-- case when else     
+select alu.NOMBREYAPELLIDO,
+	case when alu.IDLOCALIDAD is null then 'sin asignar'
+		else loc.localidad end localidad 
+        from alumno alu left join localidad loc on alu.IDLOCALIDAD = loc.IDLOCALIDAD
+        order by alu.NOMBREYAPELLIDO;
+
+select alu.NOMBREYAPELLIDO,
+		case 
+			when alu.IDLOCALIDAD is null then 'sin asignar'
+            when alu.IDLOCALIDAD = 3 then 'Capital'
+			when alu.IDLOCALIDAD = 1 then 'Capital del norte'
+            else 
+				loc.localidad end localidad from alumno alu left join localidad loc 
+                on alu.IDLOCALIDAD = loc.IDLOCALIDAD order by alu.NOMBREYAPELLIDO;
+                
+select 
+	sum( case when alu.IDLOCALIDAD = 3 then 1 else 0 end) Capital,
+	sum( case when alu.IDLOCALIDAD = 0 and alu.IDLOCALIDAD <> 3 then 1 else 0 end ) Interior
+    from alumno alu left join localidad loc on alu.IDLOCALIDAD = loc.IDLOCALIDAD
+    order by alu.NOMBREYAPELLIDO;
+-- variable
+set variable_a = 10;
+
+-- if then else
+
+/*
+CREATE DEFINER=`root`@`localhost` FUNCTION `dev_EsElCumple`(pFecha Date) RETURNS
+varchar(30) CHARSET utf8mb4
+
+BEGIN
+	declare vResultado Varchar(30);
+	declare vFechaActual Date;
+	Select curdate() into vFechaActual;
+	if extract(DAY from pFecha) = extract(DAY from curdate())
+	and extract(month from pFecha) = extract(month from curdate()) then
+	set vResultado = 'Feliz Cumpleaños!!';
+else
+set vResultado = '';
+end if;
+RETURN vResultado;
+END
+*/
+
+
+    
+CREATE TABLE `log_alumno` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dni` int DEFAULT NULL,
+  `nombreyapellido` varchar(45) DEFAULT NULL,
+  `idlocalidad` int DEFAULT NULL,
+  `idgenero` int DEFAULT NULL,
+  `fechanacimiento` date DEFAULT NULL,
+  `usuario` varchar(30) DEFAULT NULL,
+  `fechamov` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `operacion` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
